8f4ca6b52b81877663cdc2b96df226f89eb08661
Matt Plog <MPlogSWDev@outlook.com>
2024-02-12 06:09:52 -0600

    Review saving without movie details

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -5,26 +5,35 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCirclePlay } from '@fortawesome/free-solid-svg-icons';
 import {Link, useNavigate} from "react-router-dom";
 import Button from 'react-bootstrap/Button';
+import Reviews from './../reviews/Reviews';
 
 
 const Hero = ({movies}) => {
+
+    const navigate = useNavigate();
+
+    function reviews(movieId)
+    {
+        navigate(`/Reviews/${movieId}`);
+    }
+
   return (
     <div className='movie-carousel-container'>
         <Carousel>
             {
                 movies?.map((movie) =>{
-                    return(
-                        <Paper key={movie.imdbId}>
-                            <div className='movie-card-container'>
-                                <div className='movie-card' style={{"--img": `url(${movie.backdrops[0]})`}}>
-                                    <div className='movie-detail'>
-                                        <div className="movie-poster">
-                                            <img src={movie.poster} alt=""/>
-                                        </div>
-                                        <div className='movie-title'>
-                                            <h4>{movie.title}</h4>
-                                        </div>
-                                        <div className='mocie-button-container'>
+                        return(
+                    <Paper key={movie.imdbId}>
+                        <div className = 'movie-card-container'>
+                            <div className="movie-card" style={{"--img": `url(${movie.backdrops[0]})`}}>
+                                <div className="movie-detail">
+                                    <div className="movie-poster">
+                                        <img src={movie.poster} alt="" />
+                                    </div>
+                                    <div className="movie-title">
+                                        <h4>{movie.title}</h4>
+                                    </div>
+                                          <div className="movie-buttons-container">
                                         <Link to={`/Trailer/${movie.trailerLink.substring(movie.trailerLink.length - 11)}`}>
                                             <div className="play-button-icon-container">
                                                 <FontAwesomeIcon className="play-button-icon"
@@ -32,7 +41,11 @@ const Hero = ({movies}) => {
                                                 />
                                             </div>
                                         </Link>
+
+                                        <div className="movie-review-button-container">
+                                            <Button variant ="info" onClick={() => reviews(movies.imdbId)} >Reviews</Button>
                                         </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
